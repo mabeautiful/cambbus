663dedaeb0db291ddd62be885dd7991810863b4b
Narong Phath <phathnarong@gmail.com>
2016-02-16 21:05:49 -0500

    Validate username and email

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ActiveRecord::Base
-
+	validates 	:username, presence: true, length: {minimum: 3, maximum: 50}, 
+				uniqueness: { case_sensitive: false }
+	EMAIL_VALID_REGEX = /\A[^@]+@([^@\.]+\.)+[^@\.]+\z/i
+	validates :email, presence: true, uniqueness: {case_sensitive: false},
+	format: {with: EMAIL_VALID_REGEX} 
 end
