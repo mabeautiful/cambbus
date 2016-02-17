1c73da5c2e8a65b2fb8c22ba8812c38ddb555fd1
Narong Phath <phathnarong@gmail.com>
2016-02-16 20:29:00 -0500

    Add user

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160215143414) do
+ActiveRecord::Schema.define(version: 20160217004419) do
 
   create_table "posts", force: :cascade do |t|
     t.string   "title"
@@ -21,6 +21,13 @@ ActiveRecord::Schema.define(version: 20160215143414) do
     t.string   "post_type"
     t.float    "price"
     t.string   "phone"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string   "username"
+    t.string   "email"
+    t.datetime "created_at"
+    t.datetime "updated_at"
   end
 
 end
